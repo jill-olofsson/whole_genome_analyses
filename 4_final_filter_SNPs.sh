9f5d4fec6c8d8c98c72b77c8c063355f7f736058
jill-olofsson <43607539+jill-olofsson@users.noreply.github.com>
2020-07-29 09:39:23 +0200

    Update 4_final_filter_SNPs.sh

--- 4_final_filter_SNPs.sh
+++ 4_final_filter_SNPs.sh
@@ -20,8 +20,9 @@ bcftools index merged_plate01-12_ASEM_C4_v1_unique_f2_covfilt_allsites_TEmask_ch
 vcftools --gzvcf merged_plate01-12_ASEM_C4_v1_unique_f2_covfilt_allsites_TEmask_chr_max99_SNP.recode.vcf.gz --mac 56 --max-missing 0.1 --recode --out merged_plate01-12_ASEM_C4_v1_unique_f2_covfilt_allsites_TEmask_chr_max99_SNP_mac56_missing90
 bgzip merged_plate01-12_ASEM_C4_v1_unique_f2_covfilt_allsites_TEmask_chr_max99_SNP_mac56_missing90.recode.vcf
 bcftools index merged_plate01-12_ASEM_C4_v1_unique_f2_covfilt_allsites_TEmask_chr_max99_SNP_mac56_missing90.recode.vcf.gz
+	# resulting file provided in files/vcf
 
 # Step 4: thin the resulting file to only keep SNPs at least 1000 bp apart
 vcftools --gzvcf merged_plate01-12_ASEM_C4_v1_unique_f2_covfilt_allsites_TEmask_chr_max99_SNP_mac56_missing90.recode.vcf.gz --thin 1000 --recode --out merged_plate01-12_ASEM_C4_v1_unique_f2_covfilt_allsites_TEmask_chr_max99_SNP_mac56_missing90_thin1k
-
+	# resulting file provided in files/vcf
 
